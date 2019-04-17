dc24f15586d7621330fabae1cde38439f1c6ae32
Jose Castro Cisneros <jose_castro_cisneros@hotmail.es>
2019-04-17 16:03:40 -0500

    version 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('pug', () => {
     return gulp.src('src/dev/views/*.pug')
@@ -11,8 +12,7 @@ gulp.task('pug', () => {
 });
 
 gulp.task('sass', () => {
-    return gulp.src([
-        'node_modules/bootstrap/scss/bootstrap.scss',
+    return gulp.src([        
         'src/dev/scss/*.scss',
         'src/dev/scss/components/*.scss',
         'src/dev/scss/helpers/*.scss'
@@ -23,8 +23,7 @@ gulp.task('sass', () => {
 });
 
 gulp.task('js', () => {
-    return gulp.src([
-        'node_modules/bootstrap/dist/js/bootstrap-material-design.min.js',
+    return gulp.src([        
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/popper.js/dist/umd/popper.min.js'
     ])
@@ -32,15 +31,31 @@ gulp.task('js', () => {
     .pipe(browserSync.stream());
 });
 
+gulp.task('img', () =>
+    gulp.src('src/dev/img/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('src/dist/img'))
+);
+
 gulp.task('serve', ['sass', 'pug'], () => {
     browserSync.init({
         server: './src/dist'
     });
-    gulp.watch([
-        'node_modules/bootstrap/scss/bootstrap.scss',
+    gulp.watch([        
         'src/dev/scss/**/*.scss',
-        'src/dev/views/**/*.pug'
-    ], ['sass', 'pug']);
+        'src/dev/views/**/*.pug',
+        'src/dev/img/*.pug'
+    ], ['sass', 'img', 'pug']);
     gulp.watch('src/dist/*.html').on('change', browserSync.reload);
 });
 
@@ -54,4 +69,4 @@ gulp.task('fonts', () =>{
     .pipe(gulp.dest('src/dist/fonts'));
 });
 
-gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
+gulp.task('default', ['js', 'fa', 'fonts', 'serve']);
---
+++ src/dist/contact.php
@@ -0,0 +1,16 @@
+<?php
+    header('Content-type: application/json');
+
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $telefono = @trim(stripslashes($_POST['telefono']));
+    $asunto = @trim(stripslashes($_POST['asunto']));
+    $message = @trim(stripslashes($_POST['message']));
+
+    $email_from = $email;
+    $email_to = 'estudiocreativoqmart@gmail.com';
+    $body = 'Nombre: ' . $name . "\n\n" . 'E-mail: ' . $email . "\n\n" . 'Teléfono: ' . $telefono . "\n\n" . 'Asunto: ' . $asunto . "\n\n" . 'Mensaje: ' . $message;
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+    Header("Location: ./index.html");
+?>
